f0ed18e1836331ba1299eac01732014b434e6169
Андрей Дерендяев <andrei190164@gmail.com>
2022-12-12 01:10:28 +0400

    Сделать тест серий

--- main.py
+++ main.py
@@ -42,7 +42,20 @@ class GeneratorTest:
         return v
 
     def serial_test(self):
-        pass
+        n = len(self.random_numbers)
+        k = pow(self.d, 2)
+        ps = 1 / k
+        nps = n * ps
+        v = 0
+        v_list = []
+        for i in range(self.d):
+            for j in range(self.d):
+                count = 0
+                for l in range(n-1):
+                    if self.random_numbers[l] == i and self.random_numbers[l+1] == j:
+                        count += 1
+                v += pow((count - nps), 2) / nps
+        return v
 
 
 def random_multi_comparison(x):
@@ -56,16 +69,17 @@ if __name__ == '__main__':
     x = 65539
     y_list = []
     result = 0
-    for i in range(30):
+    test_count = 30
+    for i in range(test_count):
         random_numbers = []
         for j in range(1000):
            x = random_multi_comparison(x)
            #random_numbers.append(randrange(10))
            random_numbers.append(x % 10)
         generator_test = GeneratorTest(random_numbers, 10)
-        print(generator_test.frequency_test())
-        result += generator_test.frequency_test()
-    print(result / 30)
+        print(generator_test.serial_test())
+        result += generator_test.serial_test()
+    print(result / test_count)
     # lehmer_generator = LehmerGenerator(1)
     # for i in range(30):
     #     print(lehmer_generator.next())
